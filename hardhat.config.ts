e118f13bf912bde5789fa03657c6412e5af4cce6
jrocco2 <34151148+jrocco2@users.noreply.github.com>
2023-08-14 20:56:45 +1000

    Deploy script ready

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,6 +12,12 @@ const config: HardhatUserConfig = {
       },
     },
   },
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/A1kOgx6h_A2I6-pGTuuxTpE9yiA2YS0m",
+      accounts: [ "cc62d5d460834f2661b6f8f610f609682f1d0e74d25c65dc54afb35513af1304" ],
+    },
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,24 @@
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
+import { OnePlantTreasureV2, Pollen } from "../typechain-types";
 
+// npx hardhat run scripts/deploy.ts --network sepolia
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = ethers.parseEther("0.001");
+  const Pollen = await ethers.getContractFactory("Pollen");
+  const pollen: Pollen = (await Pollen.attach("0xB1b08c24dab1483254033E038B6471CB03D80d71")) as Pollen;
+  let POLLEN = await pollen.getAddress();
+  console.log("Pollen deployed to:", POLLEN);
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log("Deploying OnePlantTreasure...")
+  const OnePlantTreasure = await ethers.getContractFactory("OnePlantTreasureV2");
+  const onePlantTreasure: OnePlantTreasureV2 = (await upgrades.deployProxy(OnePlantTreasure, [POLLEN], { initializer: 'initialize' })) as unknown as OnePlantTreasureV2;
+  console.log("OnePlantTreasure deployed to:", await onePlantTreasure.getAddress());
+  // await onePlantTreasure.setBaseURI("ipfs://");
+  console.log("Transferring 7M Pollen to OnePlantTreasure...")
+  await pollen.transfer(await onePlantTreasure.getAddress(), ethers.parseEther("7000000"));
+  console.log("Opening the mint...")
+  await onePlantTreasure.setOpen(true);
+  console.log("Boom! Mint is open. You're good to go")
 }
 
 // We recommend this pattern to be able to use async/await everywhere
